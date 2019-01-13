187555a2071d7e6524affa191d8990955a6b986f
Boris Jakubík <boris.jakubik@ceskapojistovna.cz>
2019-01-13 06:38:30 +0100

    UPDATE#Project skeleton

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Header } from './components/common';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View>
+        <Header headerText="Authentification" />
+        <Text>My app!</Text>
+      </View>
+    );
+  }
+}
