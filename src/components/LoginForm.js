6c96ff64d616bc198a639b6dcffa09ab219b6db5
Boris Jakubík <jakubik@azu.sk>
2019-01-13 07:39:42 +0100

    UPDATE#Added input

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { View, TextInput } from 'react-native';
+import { Button, Card, CardSection, Input } from './common';
 
 export default class LoginForm extends Component {
+    state = { text: '' }
     render() {
         return (
             <Card>
-                <CardSection /> 
+                <CardSection>
+                    <Input
+                        label="Email"
+                        text={this.state.text}
+                        onChangeText={text => this.setState({ text })}
+                    /> 
+                </CardSection> 
                 <CardSection /> 
                 <CardSection >
                     <Button>
